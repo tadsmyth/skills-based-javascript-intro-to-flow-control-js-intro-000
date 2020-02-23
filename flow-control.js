2ff570c0482ee7a5840cf64233cab6c41393d561
Thaddeus Smith <tadsmith7@gmail.com>
2020-02-23 15:37:24 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -30,11 +30,24 @@ function ternaryTeenager(age) {
   }
 
 
-function switchAge(age) {
-switch (age >= 13 && age <= 19) {
-  case "You are a teenager":
-    return "You are a teenager";
-  default:
-    return "You have an age";
+function switchAge(age){
+    switch (age) {
+    case 13:
+        return "You are a teenager";
+    case 14:
+        return "You are a teenager";
+    case 15:
+        return "You are a teenager";
+    case 16:
+        return "You are a teenager";
+    case 17:
+        return "You are a teenager";
+    case 18:
+        return "You are a teenager";
+    case 19:
+      return "You are a teenager";
+    default:
+      return "You have an age"
+    }
   }
-}
+
